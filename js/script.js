88940fb0072f1ba2f45201942ee45aff78e30088
Quang <Quang111420@gmail.com>
2024-03-05 21:28:28 +0700

    thêm bắt buộc nhập vào form

--- js/script.js
+++ js/script.js
@@ -12,4 +12,20 @@ document.getElementById("input-form").addEventListener("submit", function(event)
       errorMessage.innerText = "";
     }
   });
-  
+  function validateForm() {
+    var name = document.getElementById('name').value;
+    var date = document.getElementById('date').value;
+    var selectedTime = document.querySelector('input[name="time"]:checked');
+    var service = document.getElementById('service').value;
+    var roomType = document.getElementById('room-type').value;
+    var firstName = document.getElementById('first-name').value;
+    var lastName = document.getElementById('last-name').value;
+    var phoneNumber = document.getElementById('phone-number').value;
+
+    if (name == '' || date == '' || !selectedTime || service == '' || roomType == '' || firstName == '' || lastName == '' || phoneNumber == '') {
+        document.getElementById('error-message').innerText = 'Please fill out all fields.';
+        return false;
+    }
+
+    return true;
+}
